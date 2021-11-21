bf2525ff1c640813c61e55183d167a83fc18074a
lukaszhanczyk <lukasz0hanczyk@gmail.com>
2021-11-21 20:11:17 +0100

    first currency migration

---
+++ src/Entity/Currency.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CurrencyRepository::class)
+ */
+class Currency
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $currency_code;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $exchange_rate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currency_code;
+    }
+
+    public function setCurrencyCode(string $currency_code): self
+    {
+        $this->currency_code = $currency_code;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?float
+    {
+        return $this->exchange_rate;
+    }
+
+    public function setExchangeRate(float $exchange_rate): self
+    {
+        $this->exchange_rate = $exchange_rate;
+
+        return $this;
+    }
+}
