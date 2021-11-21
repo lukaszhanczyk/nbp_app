e00ebfb2ffa91b6339797bf9fccca14c8a3eefdf
lukaszhanczyk <lukasz0hanczyk@gmail.com>
2021-11-21 20:26:59 +0100

    add dbService

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Services\ApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/app', name: 'app')]
-    public function index(): Response
+    /**
+     * @Route("/app", name="app")
+     **/
+    public function index(ApiService $api): Response
     {
+        $data = $api->getData();
+        dd($data);
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/AppController.php',
